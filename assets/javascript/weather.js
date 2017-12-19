a03c29f3fdbd9edbc0a442e391411ba0e20955a7
jheilhet <shirley.mramirez@yahoo.com>
2017-12-18 18:22:49 -0700

    changed weather forecast from 5-days to 3-days and deleted commented code from style.css

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -62,7 +62,8 @@ window.onload = function() {
                 // since it's a 5 day/3 hours forecast, we will 
                 // loop through data list every 8 indeces to get the data for
                 // each day for 5 days
-                for (var i = 0; i < data.list.length; i += 8) {
+                // for (var i = 0; i < data.list.length; i += 8) {
+                for (var i = 0; i <= 16; i += 8) {
                     var dayForeCast = getDayForecast(data.list[i]);
                     console.log("DayForCast: ", data.list[i]);
 
